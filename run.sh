c7f45ed9bd6a5e1b57f9a2014124057311ea786b
Xiadan <xiadan1110@gamil.com>
2021-02-03 16:36:37 +0800

    Updating log

--- run.sh
+++ run.sh
@@ -1,6 +1,11 @@
 #!/bin/sh
+echo "Reset file!"
 rm -rf node_modules
 rm package-lock.json
 rm yarn.lock
-npm i
+echo "Reseted & Start to Install"
+node -v
+npm -v
+npm install
+echo "Start to serve"
 npm run serve
